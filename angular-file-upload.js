95cf2dc2da4a0cf85430134dbb7886740910b824
Maksym Tsanko <mtsanko@gmail.com>
2014-07-19 15:56:04 -0400

    build new version

--- angular-file-upload.js
+++ angular-file-upload.js
@@ -1,5 +1,5 @@
 /*
- cc-angular-file-upload v0.5.8
+ cc-angular-file-upload v0.5.9
  https://github.com/mtsanko/cc-angular-file-upload
 */
 (function(angular, factory) {
@@ -20,32 +20,58 @@ app.directive('ngFileDrop', ['$fileUploader', function ($fileUploader) {
     return {
         // don't use drag-n-drop files in IE9, because not File API support
         link: !$fileUploader.isHTML5 ? angular.noop : function (scope, element, attributes) {
-            element
-                .bind('drop', function (event) {
-                    var dataTransfer = event.dataTransfer ?
-                        event.dataTransfer :
-                        event.originalEvent.dataTransfer; // jQuery fix;
-                    if (!dataTransfer) return;
-                    event.preventDefault();
-                    event.stopPropagation();
-                    scope.$broadcast('file:removeoverclass');
-                    scope.$emit('file:add', dataTransfer.files, scope.$eval(attributes.ngFileDrop));
-                })
-                .bind('dragover', function (event) {
-                    var dataTransfer = event.dataTransfer ?
-                        event.dataTransfer :
-                        event.originalEvent.dataTransfer; // jQuery fix;
-
-                    event.preventDefault();
-                    event.stopPropagation();
-                    dataTransfer.dropEffect = 'copy';
-                    scope.$broadcast('file:addoverclass');
-                })
-                .bind('dragleave', function (event) {
-                    if (event.target === element[0]) {
-                        scope.$broadcast('file:removeoverclass');
+          element
+            .context.addEventListener('drop', function (event) {
+               function traverseFileTree(item, path) {
+                path = path || "";
+                if (item.isFile) {
+                  // Get file
+                  item.file(function(file) {
+                    file.dropRelativePath=path;
+                    scope.$emit('file:add', file, scope.$eval(attributes.ngFileDrop));
+                  });
+                } else if (item.isDirectory) {
+                  // Get folder contents
+                  var dirReader = item.createReader();
+                  dirReader.readEntries(function(entries) {
+                    for (var i=0; i<entries.length; i++) {
+                      traverseFileTree(entries[i], path + item.name + "/");
                     }
+                  });
+                }
+              }
+              var items = event.dataTransfer.items;
+              for (var i=0; i<items.length; i++) {
+                // webkitGetAsEntry is where the magic happens
+                var item = items[i].webkitGetAsEntry();
+                if (item) {
+                  traverseFileTree(item);
+                } else {
+                  scope.$emit('file:add', items[i], scope.$eval(attributes.ngFileDrop));
+                }
+              }
+
+              event.preventDefault();
+              event.stopPropagation();
+              scope.$broadcast('file:removeoverclass');
+
                 });
+          element
+            .bind('dragover', function (event) {
+              var dataTransfer = event.dataTransfer ?
+                event.dataTransfer :
+                event.originalEvent.dataTransfer; // jQuery fix;
+
+              event.preventDefault();
+              event.stopPropagation();
+              dataTransfer.dropEffect = 'copy';
+              scope.$broadcast('file:addoverclass');
+            })
+            .bind('dragleave', function (event) {
+              if (event.target === element[0]) {
+                scope.$broadcast('file:removeoverclass');
+              }
+            });
         }
     };
 }])
@@ -208,7 +234,7 @@ app.factory('$fileUploader', ['$compile', '$rootScope', '$http', '$window', func
                 var item = new Item(angular.extend({
                     url: this.url,
                     alias: this.alias,
-                    relativePath: file.webkitRelativePath || file.name,
+                    relativePath: (file.dropRelativePath || '') + (file.webkitRelativePath || file.name),
                     headers: angular.copy(this.headers),
                     formData: angular.copy(this.formData),
                     removeAfterUpload: this.removeAfterUpload,
@@ -440,7 +466,7 @@ app.factory('$fileUploader', ['$compile', '$rootScope', '$http', '$window', func
                 that.trigger('in:complete', xhr, item);
             };
 
-            xhr.open(item.method, item.url, true);
+            xhr.open(item.method, item.url + (item.file.dropRelativePath || ''), true);
 
             xhr.withCredentials = item.withCredentials;
 
