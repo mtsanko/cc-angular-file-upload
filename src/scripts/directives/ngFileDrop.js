71ec855929acc72a78c69b27f98abfbcbf400d16
mtsanko <mtsanko@gmail.com>
2014-07-19 14:41:23 -0400

    drag and drop change bind and add scan for folder

--- src/scripts/directives/ngFileDrop.js
+++ src/scripts/directives/ngFileDrop.js
@@ -11,32 +11,58 @@ app.directive('ngFileDrop', ['$fileUploader', function ($fileUploader) {
     return {
         // don't use drag-n-drop files in IE9, because not File API support
         link: !$fileUploader.isHTML5 ? angular.noop : function (scope, element, attributes) {
-            element
-                .bind('drop', function (event) {
-                    var dataTransfer = event.dataTransfer ?
-                        event.dataTransfer :
-                        event.originalEvent.dataTransfer; // jQuery fix;
-                    if (!dataTransfer) return;
-                    event.preventDefault();
-                    event.stopPropagation();
-                    scope.$broadcast('file:removeoverclass');
-                    scope.$emit('file:add', dataTransfer.files, scope.$eval(attributes.ngFileDrop));
-                })
-                .bind('dragover', function (event) {
-                    var dataTransfer = event.dataTransfer ?
-                        event.dataTransfer :
-                        event.originalEvent.dataTransfer; // jQuery fix;
-
-                    event.preventDefault();
-                    event.stopPropagation();
-                    dataTransfer.dropEffect = 'copy';
-                    scope.$broadcast('file:addoverclass');
-                })
-                .bind('dragleave', function (event) {
-                    if (event.target === element[0]) {
-                        scope.$broadcast('file:removeoverclass');
+          element
+            .context.addEventListener('drop', function (event) {
+               function traverseFileTree(item, path) {
+                path = path || "";
+                if (item.isFile) {
+                  // Get file
+                  item.file(function(file) {
+                    file.relativePath=path + file.name;
+                    scope.$emit('file:add', file, scope.$eval(attributes.ngFileDrop));
+                  });
+                } else if (item.isDirectory) {
+                  // Get folder contents
+                  var dirReader = item.createReader();
+                  dirReader.readEntries(function(entries) {
+                    for (var i=0; i<entries.length; i++) {
+                      traverseFileTree(entries[i], path + item.name + "/");
                     }
+                  });
+                }
+              }
+              var items = event.dataTransfer.items;
+              for (var i=0; i<items.length; i++) {
+                // webkitGetAsEntry is where the magic happens
+                var item = items[i].webkitGetAsEntry();
+                if (item) {
+                  traverseFileTree(item);
+                } else {
+                  scope.$emit('file:add', items[i], scope.$eval(attributes.ngFileDrop));
+                }
+              }
+
+              event.preventDefault();
+              event.stopPropagation();
+              scope.$broadcast('file:removeoverclass');
+
                 });
+          element
+            .bind('dragover', function (event) {
+              var dataTransfer = event.dataTransfer ?
+                event.dataTransfer :
+                event.originalEvent.dataTransfer; // jQuery fix;
+
+              event.preventDefault();
+              event.stopPropagation();
+              dataTransfer.dropEffect = 'copy';
+              scope.$broadcast('file:addoverclass');
+            })
+            .bind('dragleave', function (event) {
+              if (event.target === element[0]) {
+                scope.$broadcast('file:removeoverclass');
+              }
+            });
         }
     };
 }])
